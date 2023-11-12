792418e4723e804f8a1ef20b266d0ef0df2ec4a1
Uzair Mohammed <uzair.hamed@gmail.com>
2023-11-11 21:45:01 -0800

    Added directory `js` containing file `util.js`, which contains a function to read a cookie.
    When the submit (login) button is clicked in login.html, a cookie is created with the following data: "loggedIn:true"
    
    Added an event listener to index.html which redirects user to the login page if they're not logged in (aka cookie loggedIn is null)

---
+++ public/js/util.js
@@ -0,0 +1,16 @@
+// read and return the value of a cookie (cookie name, aka "key", is provided as parameter. It's value gets returned)
+function readCookie(cname) {
+    var name = cname + "=";
+    var decoded_cookie = decodeURIComponent(document.cookie);
+    var carr = decoded_cookie.split(';');
+    for (var i = 0; i < carr.length; i++) {
+        var c = carr[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
