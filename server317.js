abbc72e08c2056f1a595e2566b6669404b2d102c
student <leonardo.marquez2021@gmail.com>
2023-12-04 22:56:58 -0800

    Changed server317.js to have post signupForm and save account settings. changed signup.html name variables to work better

--- server317.js
+++ server317.js
@@ -3,16 +3,58 @@ var message = 'CSC-317 startup template\n'
 
 var port = 3000;
 var path = require('path');
+var formidable = require('formidable');
 var express = require('express');
+var database = require('./database');
 var app = express();
 const fs = require('fs');
+const bodyParser = require('body-parser');
+let mysql = require('mysql');
 
 var StaticDirectory = path.join(__dirname, 'public');
 
 app.use(express.static(StaticDirectory));
 // Set up a route for the home page
 
+app.post('/signupForm', function (req, res) {
+    var form = new formidable.IncomingForm();
+    let connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'student',
+        password: 'student',
+        database: 'plannerzdb'
+        });
+    form.parse(req, function (err, ffields, files) {
+        connection.connect(function(err) {
+            if (err) throw err;
+            let qq = "INSERT INTO USER (firstName, lastName, email, password) VALUES('" + ffields.firstName +"', '" + ffields.lastName + "', '" + ffields.loginEmail + "', '" + ffields.password +"')";
+            console.log(qq);
+            connection.query(qq, function(err, result, fields) {
+                if(err) throw err;
+                var resstr = '<form action="signupForm method="post" enctype="multipart/form-data">'
+                console.log(result);
+                qq = "INSERT INTO USERSETTINGS (owner_id, theme, language) VALUES ("+ result.insertId + ", 'light', 'en')";
+                connection.query(qq,function(err, result, fields){
+                    if(err) throw err;
+                    resstr = resstr + "<br>" + qq;
+                    console.log(resstr);
+                    return res.send(resstr);
+                });
+            })
 
+        })
+    })
+})
+
+app.post("/saveAccountSettings", function(req,res) {
+    console.log(req.body);
+    // database.saveAccountSettings;
+});
+
+app.post("/savePreferenceSettings", function(req,res) {
+    console.log(req,body);
+    // database.savePreferenceSettings;
+});
 
 app.listen(port, () => {
     console.log(`Listening on http://localhost:${port}/`);
